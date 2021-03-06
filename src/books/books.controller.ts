7996260526901f7a47a6ef7f8fb5363949a7cb97
activizator <activizator@ya.ru>
2021-03-06 13:14:37 +0300

    add book and find books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Book } from './interfaces/book.interface';
 
-@Controller('books')
+@Controller('api/books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
-  @Get('api')
-  getHello(): string {
-    return this.booksService.getHi();
+  @Get()
+  getBooks(): Book[] {
+    return this.booksService.findAll();
+  }
+
+  @Post()
+  pushBook(@Body() book: Book): Book {
+    return this.booksService.create(book);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from './interfaces/book.interface';
 
 @Injectable()
 export class BooksService {
-  getHi(): string {
-    return 'Hi!';
+  private readonly books: Book[] = [];
+
+  create(book: Book) {
+    const { title, description, authors, favorite, fileCover, fileName } = book;
+    this.books.push({
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName,
+    });
+    return book;
+  }
+
+  findAll(): Book[] {
+    return this.books;
   }
 }
