86f80d668079d318d6b318d0940872ac968d7258
activizator <activizator@ya.ru>
2021-03-07 10:46:01 +0300

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksService } from './books/books.service';
-import { BooksController } from './books/books.controller';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  imports: [BooksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
