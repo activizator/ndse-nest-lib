0931c4adf435bf6658119041cdc02b23925303a6
activizator <activizator@ya.ru>
2021-04-04 11:09:32 +0300

    release book comment service & controller

--- src/book.comment/book.comment.controller.ts
+++ src/book.comment/book.comment.controller.ts
@@ -1,4 +1,37 @@
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { BookCommentService } from './book.comment.service';
+import { BookComment } from './interfaces/book.comment.interface';
 
-@Controller('book.comment')
-export class BookCommentController {}
+@Controller('api/book/comment')
+export class BookCommentController {
+  constructor(private readonly bookCommentService: BookCommentService) {}
+
+  @Get(':bookId')
+  getComments(@Param() bookId: string): Promise<BookComment[]> {
+    return this.bookCommentService.findAllBookComment(bookId);
+  }
+
+  @Get(':id')
+  getComment(@Param() id: number): Promise<BookComment> {
+    return this.bookCommentService.read(id);
+  }
+
+  @Post()
+  pushComment(@Body() comment: BookComment): Promise<BookComment> {
+    return this.bookCommentService.create(comment);
+  }
+
+  @Put(':id')
+  upsertComment(
+    @Param() id: number,
+    @Body() comment: BookComment,
+  ): Promise<BookComment> {
+    return this.bookCommentService.upsert(id, comment);
+  }
+
+  @Delete(':id')
+  deleteComment(@Param() id: number): Promise<BookComment> {
+    return this.bookCommentService.delete(id);
+  }
+}
--- src/book.comment/book.comment.service.ts
+++ src/book.comment/book.comment.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { BookComment } from './interfaces/book.comment.interface';
+import { BookCommentDocument } from './schemas/book.comment.schema';
 
 @Injectable()
-export class BookCommentService {}
+export class BookCommentService {
+  constructor(
+    @InjectModel('BookComment')
+    private bookCommentModel: Model<BookCommentDocument>,
+  ) {}
+
+  async findAllBookComment(bId: string): Promise<BookComment[] | null> {
+    return await this.bookCommentModel.find({ bookId: bId });
+  }
+
+  async create(comm: BookComment): Promise<BookComment | null> {
+    const { id, bookId, comment } = comm;
+    return await this.bookCommentModel.create({
+      id,
+      bookId,
+      comment,
+    });
+  }
+
+  async read(i: number): Promise<BookComment | null> {
+    return await this.bookCommentModel.findOne({ id: i });
+  }
+
+  async upsert(i: number, comm: BookComment): Promise<BookComment | null> {
+    const { id, bookId, comment } = comm;
+    return await this.bookCommentModel.findOneAndUpdate(
+      { id: i },
+      {
+        id,
+        bookId,
+        comment,
+      },
+      { upsert: true },
+    );
+  }
+
+  async delete(i: number): Promise<BookComment | null> {
+    return await this.bookCommentModel.findOneAndDelete({ id: i });
+  }
+}
